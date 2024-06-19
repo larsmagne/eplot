de091cfa5775f0d95112e2e3e700c2a39bbd5f23
Lars Ingebrigtsen <larsi@gnus.org>
2024-06-19 17:18:23 +0200

    Add standard scaffolding

---
+++ eplot.el
@@ -0,0 +1,27 @@
+;;; eplot.el --- Manage and Edit Wordpress Posts -*- lexical-binding: t -*-
+
+;; Copyright (C) 2024 Free Software Foundation, Inc.
+
+;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
+;; Keywords: charts
+;; Package: eplot
+;; Version: 1.0
+;; Package-Requires: ((emacs "29.0.59"))
+
+;; eplot is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;;; Commentary:
+
+;; The main entry point is `M-x eplot' in a buffer with time series
+;; data.
+
+;;; Code:
+
+(require 'cl-lib)
+
+(provide 'eplot)
+
+;;; eplot.el ends here
