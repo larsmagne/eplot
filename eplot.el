6b41b0af94c0f42766ed1af2b6b70d75f99fc44a
Lars Ingebrigtsen <larsi@gnus.org>
2024-06-21 23:25:24 +0200

    Allow plotting color circle fills

--- eplot.el
+++ eplot.el
@@ -499,7 +499,10 @@
 			      period)))))))
 
 (defun eplot--vary-color (color n)
-  (let ((colors '("red" "green" "blue" "cyan" "yellow" "purple")))
+  (let ((colors ["#e6194b" "#3cb44b" "#ffe119" "#4363d8" "#f58231" "#911eb4"
+		 "#46f0f0" "#f032e6" "#bcf60c" "#fabebe" "#008080" "#e6beff"
+		 "#9a6324" "#fffac8" "#800000" "#aaffc3" "#808000" "#ffd8b1"
+		 "#000075" "#808080" "#ffffff" "#000000"]))
     (if (not (equal color "vary"))
 	color
       (elt colors (mod n (length colors))))))
@@ -580,7 +583,10 @@
 			     :stroke color))))
 	      (circle
 	       (svg-circle svg px py (eplot--vn 'size headers 3)
-			   :stroke color))
+			   :stroke color
+			   :fill (eplot--vary-color
+				  (eplot--vs 'fill headers "none")
+				  x)))
 	      (cross
 	       (let ((s (eplot--vn 'size headers 3)))
 		 (svg-line svg (- px s) (- py s)
