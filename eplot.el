d8ea9f31bb1cae7089deceb8d19e44cbc0233c9b
Vagn Johansen <vagn.johansen@danfoss.com>
2024-06-28 08:22:50 +0200

    Support Emacs 28 by passing image-scaling-factor to image-compute-scaling-factor
    In Emacs 29 the first argument is optional and defaults to image-scaling-factor

--- eplot.el
+++ eplot.el
@@ -676,7 +676,7 @@ If RETURN-IMAGE is non-nil, return it instead of displaying it."
       ;; Set the size of the chart based on the window it's going to
       ;; be displayed in.  It uses the *eplot* window by default, or
       ;; the current one if that isn't displayed.
-      (let ((factor (image-compute-scaling-factor)))
+      (let ((factor (image-compute-scaling-factor image-scaling-factor)))
 	(unless width
 	  (setq width (/ (* (window-pixel-width
  			     (get-buffer-window "*eplot*" t))
